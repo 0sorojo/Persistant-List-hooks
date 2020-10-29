d6891c22e975c16a6d463d93e711452a2ec64e58
0sorojo <redrootproductions@gmail.com>
2020-10-29 16:11:55 -0400

    alert functionality and inline conditional display

--- src/Alert.js
+++ src/Alert.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Alert = () => {
+const Alert = ({ type, msg, removeAlert }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
-      <h2>ALERT</h2>
+      <p className={`alert alert-${type}`}>{msg}</p>
     </>
   );
 };
--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ const App = () => {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
       // alert
+      showAlert(true, 'danger', 'please enter accepted value');
     } else if (name && isEditing) {
       // edit
     } else {
@@ -23,32 +28,42 @@ const App = () => {
     }
   };
 
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+
   return (
-    <section className='section-center'>
-      <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
-        <h3>Persistant List</h3>
-        <div className='form-control'>
-          <input
-            type='text'
-            className='grocery'
-            placeholder='e.g. eggs'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button type='submit' className='submit-btn'>
-            {isEditing ? 'edit' : 'submit'}
-          </button>
-        </div>
-        <button>submit</button>
-      </form>
-      {list.length > 0 && (
-        <div className='grocery-container'>
-          <List items={list} />
-          <button className='clear-btn'>clear-items</button>
-        </div>
-      )}
-    </section>
+    <>
+      <section className='section-center'>
+        {alert.show ? (
+          <Alert {...alert} removeAlert={showAlert} />
+        ) : (
+          <p className='alert'></p>
+        )}
+        <form className='grocery-form' onSubmit={handleSubmit}>
+          <h3>Persistant List</h3>
+          <div className='form-control'>
+            <input
+              type='text'
+              className='grocery'
+              placeholder='e.g. eggs'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type='submit' className='submit-btn'>
+              {isEditing ? 'edit' : 'submit'}
+            </button>
+          </div>
+          <button>submit</button>
+        </form>
+        {list.length > 0 && (
+          <div className='grocery-container'>
+            <List items={list} />
+            <button className='clear-btn'>clear-items</button>
+          </div>
+        )}
+      </section>
+    </>
   );
 };
 
